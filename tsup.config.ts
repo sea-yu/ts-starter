6a3e10e6bbcbe1c39e920ae4a92d53eff06ee54d
seayu <yhyuhai@163.com>
2022-06-29 17:14:36 +0800

    choit: jest => vitest pkgroll => tsup

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  dts: true,
+  minify: true,
+  clean: true,
+  format: [
+    'cjs',
+    'esm',
+  ],
+})
